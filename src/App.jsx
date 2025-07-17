6c65e70b6690af457605564f8d2bd01a1f1b2938
MRasool05 <ce.mrj2005@gmail,com>
2025-07-18 02:01:22 +0330

    first step

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <menu>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">player2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+
+      </div>
+    </menu>
+  )
+}
+
+export default App
